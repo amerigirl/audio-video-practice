761bf60b7069b8d11bfeaa6adfae7e96fe45ae08
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-10-27 19:32:28 -0700

    added raw video and audio samples; mostly setup video

--- app/src/main/java/com/example/audiovideoexample/VideoActivity.kt
+++ app/src/main/java/com/example/audiovideoexample/VideoActivity.kt
@@ -1,6 +1,10 @@
 package com.example.audiovideoexample
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,22 @@ class VideoActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //create buttons
+        val videoView = findViewById<VideoView>(R.id.videoView)
+
+        val btnPlayVideo = findViewById<Button>(R.id.btnPlayVideo)
+
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        //show file path
+        val uri = Uri.parse("android.resource://$packageName/${R.raw.sample_video}")
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+
+        btnPlayVideo.setOnClickListener {
+            videoView.start()
         }
     }
 }
